ced0b54a3d5b1cc9500c49d1653e43351ca1de5a
yunju <>
2013-03-15 11:50:22 +0000

    for file location

--- ForCheck/AnaCheck/AnalyzerCheck/test/analyzercheck_cfg.py
+++ ForCheck/AnaCheck/AnalyzerCheck/test/analyzercheck_cfg.py
@@ -10,7 +10,10 @@ process.source = cms.Source("PoolSource",
     # replace 'myfile.root' with the source file you want to use
     fileNames = cms.untracked.vstring(
         #'file:/data4/yunju/VBF2012/skim2l2q/SkimPAT_H2l2q_523_v3_l_GluGluToHToZZTo2L2Q_M-200h2l2qSkimData_22_1_2KD.root'
-       'file:/data4/yunju/VBF2012/SkimPAT_H2l2q_533_v1_GluGluToHToZZTo2L2Q_M-200_h2l2qSkimData_2_1_5SE_2nd.root'
+       #'file:/afs/cern.ch/work/y/yunju/public/SkimPAT_H2l2q_523_v3_l_GluGluToHToZZTo2L2Q_M-200h2l2qSkimData_22_1_2KD.root'
+      'file:/afs/cern.ch/work/y/yunju/public/SkimPAT_H2l2q_533_v1_GluGluToHToZZTo2L2Q_M-200_h2l2qSkimData_2_1_5SE_2nd.root'
+       
+
     )
 )
 
